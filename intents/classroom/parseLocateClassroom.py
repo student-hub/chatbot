d771e8f474c54834ed578c9ae6b6eefe1ae088f5
Anca David <david.anca97@gmail.com>
2021-03-18 22:06:47 +0200

    Changed locateEvent to locateClassroom.

--- intents/classroom/parseLocateClassroom.py
+++ intents/classroom/parseLocateClassroom.py
@@ -1,10 +1,10 @@
 import json
 
-with open('intentLocateEvent.json') as intentLocateEvent:
-	intent = json.load(intentLocateEvent)
+with open('intentLocateClassroom.json') as intentLocateClassroom:
+	intent = json.load(intentLocateClassroom)
 with open('../resources/classrooms.json') as classroomModel:
 	classroom = json.load(classroomModel)
-f = open("generatedIntentLocateEvent.yml", "w")
+f = open("generatedIntentLocateClassroom.yml", "w")
 
 for i in intent:
 	for c in classroom:
