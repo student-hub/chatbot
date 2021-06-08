7617a88b15f8e578d4bc8e8ed188c6fe6e5bbaad
Gabriel Gavriluta <gavriluta.gabriel@gmail.com>
2021-06-08 23:11:39 +0300

    add studentInfo intent

---
+++ intents/user/parse_student_info.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+with open('intentInitStudentInfoRO.json') as intentSRLeaderRO:
+    intentRO = json.load(intentSRLeaderRO)
+with open('intentInitStudentInfoEN.json') as intentSRLeaderEN:
+    intentEN = json.load(intentSRLeaderEN)
+with open('../../../resources/nrSR.json') as nrSRModel:
+    nrSR = json.load(nrSRModel)
+f = open("generatedIntentSRLeaderRO.yml", "w")
+fEN = open("generatedIntentSRLeaderEN.yml", "w")
+
+for i in intentRO:
+    for n in nrSR:
+        f.write(i.replace("Sx", nrSR[n]['nameSR']) + "\n")
+
+for i in intentEN:
+    for n in nrSR:
+        fEN.write(i.replace("Sx", nrSR[n]['nameSR']) + "\n")
+
+f.close()
+fEN.close()
