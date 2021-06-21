47adcd3e2c0573003dbd194de60d064086dd4af0
Gabriel Gavriluta <gavriluta.gabriel@gmail.com>
2021-06-21 17:06:41 +0300

    config basic locate event

--- src/actions/actions.py
+++ src/actions/actions.py
@@ -6,10 +6,16 @@
 
 
 from typing import Any, Text, Dict, List
-
+import classroomLocation
+import pandas as pd
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-
+import firebase_admin
+from firebase_admin import credentials, firestore, auth
+cred = credentials.Certificate("acs-upb-mobile-dev-firebase-adminsdk-mgl5c-9617ffc92e.json")
+firebase_admin.initialize_app(cred)
+firestore_db = firestore.client()
+snapshots = firestore_db.collection(u'users');
 
 class ActionLocateClassroom(Action):
 
@@ -20,8 +26,18 @@ class ActionLocateClassroom(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text="Sala este locatalizată...")
-
+        entity = tracker.latest_message['entities'][0]['value']
+        #remove whitespaces from string and make capital letters
+        entity = entity.replace(" ", "").upper()
+    
+        location = classroomLocation.getClassroomLocation(entity)
+        if(len(location)):
+            if(location["floor"] > 0):
+                dispatcher.utter_message(text="Sala este locatalizată în " + location["building"] + ", etajul " + str(location["floor"]))
+            else:
+                dispatcher.utter_message(text="Sala este locatalizată în " + location["building"] + ", la parter")
+        else:
+            dispatcher.utter_message(text="Sala căutată nu a fost găsită, te rog să verifici dacă ai scris corect")
         return []
 
 class ActionGetDateEvent(Action):
@@ -33,7 +49,45 @@ class ActionGetDateEvent(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text="Data evenimentului este...")
+        doc = snapshots.document(u"ceHKuicm86eCv2TVTCFkxKBzZvi2").get().to_dict()
+        print(doc)
+        id = tracker.current_state()['sender_id']
+        classFields = doc["class"];
+        event = ""
+        entity = ""
+        try:
+            entity = tracker.latest_message['entities'][0]['value']
+        except:
+            dispatcher.utter_message(text="Scuze, nu dețin această informație. Te rog verifică daca numele evenimentului este scris corect!")
+            return []
+        event = "L" if classFields[0] == "BSc" else "M"
+        words = classFields[3].split('-')
+        year = words[0]
+        serie = words[1]
+        event = event + "-A" + year + "-S1"
+        if(len(entity) < 6):
+            event = event + "-" + entity.upper() + "-" + serie
+        else:
+            abreviation = ""
+            for x in entity.split():
+                if x[0].isalpha():
+                    abreviation = abreviation + x[0]
+            if(entity[-1].isnumeric()):
+                abreviation = abreviation + entity[-1]
+            event = event + "-" + abreviation.upper() + "-" + serie
+        print(event)
+        events = firestore_db.collection(u'events').get()
+        for e in events:
+            currentEvent = e.to_dict()
+            if "class" in currentEvent.keys():
+                if currentEvent["class"] == event:
+                    print(currentEvent["start"])
+                    temp = pd.Timestamp(currentEvent["start"])
+                    day = temp.day_name()
+                    hour = temp.hour + 3 #timezone
+                    minute = temp.minute
+                    h = str(hour) + str(minute) if minute > 0 else str(hour)
+        dispatcher.utter_message(text=entity + " are loc " + day + ", la ora " + h)
 
         return []
 
@@ -131,7 +185,7 @@ class ActionGetGroupLeaderName(Action):
 class ActionGetSRLeaderName(Action):
 
     def name(self) -> Text:
-        return "aaction_get_SR_leader_name"
+        return "action_get_SR_leader_name"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
@@ -140,4 +194,3 @@ class ActionGetSRLeaderName(Action):
         dispatcher.utter_message(text="Numele șefului de serie este...")
 
         return []
-
