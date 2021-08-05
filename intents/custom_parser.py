8198d8557d22030180a5aef1e09f6eb134987fa9
Radu1999 <rchivereanu@gmail.com>
2021-08-06 00:37:36 +0300

    doesnt support shuffling and people parsing

--- intents/custom_parser.py
+++ intents/custom_parser.py
@@ -12,45 +12,76 @@ def write_replaced_intents(file, raw_intent, data, key_info, replaceable_key, re
         for subj in data[key_info]:
             file.write(entry.replace(replaceable_key, subj[replace_key]) + "\n")
 
-def main():
 
-    languages = [
-        'RO',
-        'EN'
-    ]
+languages = [
+    'RO',
+    'EN'
+]
 
-    intent_raw_filename = [
-        'intentLocateClassroom'
-    ]
+intent_relative_path = [
+    ['classroom/'],
+    [
+        'event/date/',
+        'event/endTime/',
+        'event/grading/',
+        'event/locateEvent/',
+        'event/minConditions/',
+        'event/minNrOfAttendings/',
+        'event/startTime/'
+    ],
+    ['people/student/groupLeader']
 
-    intent_replaced_filename = [
-        'generatedIntentLocateClassroom',
-    ]
+]
 
-    database_entry = [
-        '../resources/classroomsFirebase.json'
-    ]
 
-    key_info = [
-        'classrooms',
-    ]
+intent_raw_filename = [
+    ['intentLocateClassroom'],
+    [
+        'intentGetDateEvent',
+        'intentEndTimeEvent',
+        'intentCourseGrading',
+        'intentLocateEvent',
+        'intentMinConditions',
+        'intentMinNrOfAttendings',
+        'intentStartTimeEvent'
+    ],
+]
 
-    replacing_key = [
-        'name',
-    ]
+database_entry = [
+    'classroomsFirebase',
+    'classesFirebase',
+]
 
-    replaceable_key = [
-        'Cx',
-    ]
 
-    for index in range(len(intent_raw_filename)):
-        for lang in languages:
-            raw_intent = load_from_file(intent_raw_filename[index] + lang + '.json')
-            write_file = open(intent_replaced_filename[index] + lang + '.yml') 
-            data = load_from_file(database_entry[index])
-            write_replaced_intents(write_file, raw_intent, data,
-                    key_info[index], replaceable_key[index],
-                    replacing_key[index])
+
+key_info = [
+    'classrooms',
+    'classes',
+]
+
+replacing_key = [
+    ['name'],
+    ['name', 'shortname'],
+]
+
+replaceable_key = [
+    'Cx',
+    'Ex',
+]
+
+for index in range(len(intent_raw_filename)):
+    
+    data = load_from_file('resources/' + database_entry[index] + '.json')
+    for lang in languages:
+        for it in range(len(intent_raw_filename[index])): 
+            raw_intent = load_from_file(
+                intent_relative_path[index][it] + intent_raw_filename[index][it] + lang + '.json')
+            write_file = open(intent_relative_path[index][it] + 'generated' +
+                intent_raw_filename[index][it].capitalize() + lang + '.yml', "w") 
+            for rpl_key in replacing_key[index]:
+                write_replaced_intents(write_file, raw_intent, data,
+                        key_info[index], replaceable_key[index],
+                        rpl_key)
             write_file.close()
 
 
