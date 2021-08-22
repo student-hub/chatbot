38c6588df2094e4d99493c54c9b669af6f3cdd32
Radu1999 <rchivereanu@gmail.com>
2021-08-22 20:03:06 +0300

    works but without shuffle

--- intents/custom_parser.py
+++ intents/custom_parser.py
@@ -1,19 +1,27 @@
 import json
-from os import write
-import shutil
+import os
 
 def load_from_file(file_name):
     with open(file_name) as file:
         return json.load(file)
 
+def convertJsonToWYaml(json_file, yaml_file):
+    js = json.load(json_file)
+    for entry in js:
+        yaml_file.write(entry + "\n")
+    
 
 def write_replaced_intents(file, raw_intent, data, pair):
+    file.write("[" + "\n")
     for entry in raw_intent:       
         for name in data:
             if not isinstance(data[name], list):
                 data[name] = [data[name]]
             for subj in data[name]:
-                file.write(entry.replace(pair[0], subj[pair[1]]) + "\n")
+                file.write(f'"' + entry.replace(pair[0], subj[pair[1]]) + f'",'+ "\n")
+    file.seek(file.tell() - 2, os.SEEK_SET)
+    file.truncate()
+    file.write("\n" + "]")
 
 def clear_files(intent_raw_filename, intent_relative_pat, languages):
 
@@ -50,7 +58,9 @@ intent_relative_path = [
         'event/minNrOfAttendings/',
         'event/startTime/'
     ],
-    ['people/student/groupLeader/']
+    ['people/student/groupLeader/'],
+    ['people/student/srLeader/'],
+    ['people/teacher/']
 
 ]
 
@@ -75,7 +85,9 @@ intent_raw_filename = [
         'intentMinNrOfAttendings',
         'intentStartTimeEvent'
     ],
-    ['intentGroupLeader']
+    ['intentGroupLeader'],
+    ['intentSRLeader'],
+    ['intentGetTeacherName']
 
 ]
 
@@ -85,7 +97,9 @@ database_entry = [
     ['classroomsFirebase'],
     ['classesFirebase'],
     ['classesFirebase'],
-    ['nrGroup', 'nrSR']
+    ['nrGroup', 'nrSR'],
+    ['nrSR'],
+    ['classroomsFirebase']
 ]
 
 replacing_dict = [
@@ -104,11 +118,16 @@ replacing_dict = [
     {
         'Gx' : 'nameGroup',
         'Sx' : 'nameSR',
+    },
+
+    {
+        'Sx' : 'nameSR'
+    },
+    {
+        'Cx' : 'name'
     }
 
 ]
-
-#to do: generate the intermediary file as json
 
 for index in range(len(intent_raw_filename)):
     
@@ -116,18 +135,21 @@ for index in range(len(intent_raw_filename)):
     for lang in languages:
         for it in range(len(intent_raw_filename[index])): 
             path = intent_relative_path[index][it] + intent_raw_filename[index][it] + lang + '.json'
+            raw_intent = load_from_file(path)          
+            path = 'temp.json'
             for (key, value), database_name in zip(replacing_dict[index].items(), database_entry[index]):
-                data = load_from_file('./resources/' + database_name + '.json')
-                if(database_name == 'nrSR'):
-                    print("here")
+                data = load_from_file('./resources/' + database_name + '.json')           
+                temp_file = open("temp.json", "w")             
+                write_replaced_intents(temp_file, raw_intent, data, (key, value))
+                temp_file.close()
                 raw_intent = load_from_file(path)
-                if(database_name == 'nrSR'):
-                    print("here")
-                write_file = open(intent_relative_path[index][it] + 'generated' +
-                intent_raw_filename[index][it].capitalize() + lang + '.yml', "a")
-                write_replaced_intents(write_file, raw_intent, data, (key, value))
-                path = intent_relative_path[index][it] + 'generated' + \
-                    intent_raw_filename[index][it].capitalize() + lang + '.yml'
+                
+                
+
+            write_file = open(intent_relative_path[index][it] + 'generated' +
+                intent_raw_filename[index][it].capitalize() + lang + '.yml', "w")
+            temp_file = open("temp.json")
+            convertJsonToWYaml(temp_file, write_file)
             write_file.close()
 
 
