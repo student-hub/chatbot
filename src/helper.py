38937a8224c4522826e0654cb6eb6304c41a53b2
Anca David <david.anca97@gmail.com>
2022-02-04 19:38:26 +0200

    Add rasa deployment.

--- src/helper.py
+++ src/helper.py
@@ -108,7 +108,7 @@ def compute_event_name(classFields, eventEntity):
     month = formatted_today[0]
 
     if month in ["February", "March", "April", "May", "June"]:
-        event = event + "-A" + year + "-S2"
+        event = event + "-A" + year + "-S1"
     else:
         event = event + "-A" + year + "-S1"
 
