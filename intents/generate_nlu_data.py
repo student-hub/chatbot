aab6b73f238e9cda991e4c1f962ce3b7c06c2338
Anca David <david.anca97@gmail.com>
2022-03-04 20:58:11 +0200

    Generate nlu data file with all the faqs.

---
+++ intents/generate_nlu_data.py
@@ -0,0 +1,26 @@
+import sys
+import ruamel.yaml
+import os
+
+nlu_file = open("nlu_data.yml", "w")
+nlu_file.write("version: '3.0'\n")
+nlu_file.write("nlu:\n")
+
+path = ''
+yaml = ruamel.yaml.YAML()
+
+if sys.argv[1] == "en":
+    print("en")
+    path = "faq\en"
+else:
+    print("ro")
+    path = "faq\\ro"
+
+for faq_file in os.listdir(path):
+    if faq_file.endswith(".yml"):
+        file_path = f"{path}\{faq_file}"
+        with open(file_path, 'r') as intent_faq:
+            intent = yaml.load(intent_faq)
+            yaml.dump(intent, nlu_file)
+       
+nlu_file.close()
