ecbc27fd3edb3cc71bb75714a3966eb5efd98257
Anca David <david.anca97@gmail.com>
2022-03-12 17:12:27 +0200

    Add intents with ent in the final file.

--- intents/generate_nlu_data.py
+++ intents/generate_nlu_data.py
@@ -2,6 +2,11 @@ import sys
 import ruamel.yaml
 import os
 
+def dump_intent(file_path, nlu_file):
+    with open(file_path, 'r') as intent_faq:
+        intent = yaml.load(intent_faq)
+        yaml.dump(intent, nlu_file)
+
 nlu_file = open("nlu_data.yml", "w")
 nlu_file.write("version: '3.0'\n")
 nlu_file.write("nlu:\n")
@@ -17,8 +22,8 @@ else:
 for faq_file in os.listdir(path):
     if faq_file.endswith(".yml"):
         file_path = f"{path}\{faq_file}"
-        with open(file_path, 'r') as intent_faq:
-            intent = yaml.load(intent_faq)
-            yaml.dump(intent, nlu_file)
+        dump_intent(file_path, nlu_file)
        
+
+dump_intent("./generatedIntents.yml", nlu_file)
 nlu_file.close()
